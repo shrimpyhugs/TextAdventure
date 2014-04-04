1886b5124b5a3074e350f47c58905ead9d05b168
Dylan Hughes <sayabodohproductions@gmail.com>
2014-04-05 01:09:17 +1100

    Profession Update
    Classes now changed to "Professions" and updated to not be randomly
    selected from a list to displaying all Professions

--- TextAdventure/src/textadventure/CharacterInfo.java
+++ TextAdventure/src/textadventure/CharacterInfo.java
@@ -9,8 +9,51 @@ package textadventure;
 import java.util.ArrayList;
 
 public class CharacterInfo {
-String name,weapon,profession;
-int spells;
-int STRENGTH,DEXTERITY,MAGIC,STEALTH,INFLUENCE,FOOD;
-ArrayList<String> Equiptment;
+    String name,weapon,profession;
+    int STRENGTH,DEXTERITY,MAGIC,STEALTH,INFLUENCE,FOOD,spells;
+    ArrayList<String> Equiptment;
+public void create(){
+spells = 0;
+STRENGTH = (int)(Math.random() * 8) + 9; //gives random stat between 7 and 16
+DEXTERITY = (int)(Math.random() * 8) + 9; //gives random stat between 7 and 16
+MAGIC = (int)(Math.random() * 8) + 9; //gives random stat between 7 and 16
+STEALTH = (int)(Math.random() * 8) + 9; //gives random stat between 7 and 16
+INFLUENCE = (int)(Math.random() * 8) + 9; //gives random stat between 7 and 16
+FOOD = 4;
+
+    switch(profession) { //Check Profession and apply Specialisations
+        case "Sorceror" : 
+                          MAGIC = MAGIC + 4;
+                          spells = spells + 2;
+                          STRENGTH = STRENGTH - 2;
+                          DEXTERITY = DEXTERITY - 2;
+            break;
+        case "Rogue"    : 
+                          STEALTH = STEALTH + 3;
+                          DEXTERITY = DEXTERITY + 2;
+                          MAGIC = MAGIC - 2;
+                          INFLUENCE = INFLUENCE - 2;
+                          
+            break;
+        case "Bard"     : 
+                          INFLUENCE = INFLUENCE + 4;
+                          STRENGTH = STRENGTH - 1;
+                          STEALTH = STEALTH - 2;
+                          Equiptment.add("Instrument");
+            break;
+        case "Warrior"  : 
+                          STRENGTH = STRENGTH + 4;
+                          DEXTERITY = DEXTERITY + 1;
+                          MAGIC = MAGIC - 2;
+                          STEALTH = STEALTH - 2;
+            break;
+        case "Chef"     : 
+                          FOOD = FOOD + 4;
+                          INFLUENCE = INFLUENCE + 1;
+                          STEALTH = STEALTH - 2;
+                          MAGIC = MAGIC - 2;
+            break;
+        }
+
+}
 }
--- TextAdventure/src/textadventure/TextAdventure.java
+++ TextAdventure/src/textadventure/TextAdventure.java
@@ -11,6 +11,7 @@ public class TextAdventure {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); //your everyday run of the mill Scanner
         CharacterInfo Character = new CharacterInfo(); //Create Character
+      
         //**************************choices****************************//
         String[] Weapon = new String[5];{
             Weapon[0] = "Frying Pan";
@@ -36,85 +37,71 @@ public class TextAdventure {
             Monster[5] = "Possessed Significant Other"; 
         }
         //*************************************************************//
+        
     int a,b,c; //declares options
     System.out.println(">>Hello Adventurer, What is your Name?");
     Character.name = input.nextLine(); //sets player name
-    System.out.println(">>Welcome " + Character.name + " ,what is your weapon of choice?");
-    a = (int)(Math.random() * 5); //sets random item for weapon option a
-    b = (int)(Math.random() * 5); //sets random item for weapon option b
-    c = (int)(Math.random() * 5); //sets random item for weapon option c
-    while(b == a){b = (int)(Math.random() * 5);} //make sure b is not the same as a
-    while(c == a || c == b){c = (int)(Math.random() * 5);} //make sure c is not the same as a or b     
-    System.out.println(">>  a) " + Weapon[a] + " b) " + Weapon[b] + " c) " + Weapon[c]);//shows choices
+    System.out.println(">>Welcome " + Character.name + " , choose a class:");
+    System.out.println(">> a)Sorceror b) Rogue c) Bard d) Warrior e) Chef");
     int select = 0;
     while(select == 0){ //while option hasnt been selected
         switch(input.next()) { //test to see which option was selected
-            case "a": Character.weapon = Weapon[a]; //if option A, set A as weapon
+            case "a": Character.profession = Profession[0]; //Set Sorceror Profession
                       select = 1;  //option has been selected
                       break;
-            case "b": Character.weapon = Weapon[b]; //if option B, set B as weapon
+            case "b": Character.profession = Profession[1]; //Set Rogue Profession
+                     select = 1;  //option has been selected
+                      break;
+            case "c": Character.profession = Profession[2]; //Set Bard Profession
                       select = 1;  //option has been selected
                       break;
-            case "c": Character.weapon = Weapon[c]; //if option C, set C as weapon
+            case "d": Character.profession = Profession[3]; //Set Bard Profession
                       select = 1;  //option has been selected
                       break;
-        }
-    }
-    System.out.println(">>you have chosen the " + Character.weapon + " and what class are you?");
-    
-    a = (int)(Math.random() * 5); //sets random item for weapon option a
-    b = (int)(Math.random() * 5); //sets random item for weapon option b
-    c = (int)(Math.random() * 5); //sets random item for weapon option c
-    while(b == a){b = (int)(Math.random() * 5);} //make sure b is not the same as a
-    while(c == a || c == b){c = (int)(Math.random() * 5);} //make sure c is not the same as a or b     
-    System.out.println(">>  a) " + Profession[a] + " b) " + Profession[b] + " c) " + Profession[c]);//shows choices
-    select = 0;
-    while(select == 0){ //while option hasnt been selected
-        switch(input.next()) { //test to see which option was selected
-            case "a": Character.profession = Profession[a]; //if option A, set A as weapon
-                      select = 1;  //option has been selected
-                      break;
-            case "b": Character.profession = Profession[b]; //if option B, set B as weapon
-                      select = 1;  //option has been selected
-                      break;
-            case "c": Character.profession = Profession[c]; //if option C, set C as weapon
+            case "e": Character.profession = Profession[4]; //Set Bard Profession
                       select = 1;  //option has been selected
                       break;
         }
-    }
-    
+    //************************************RANDOM EVENT CHOICE*************************************************************//
+    //a = (int)(Math.random() * 5); //sets random item for weapon option a
+    //b = (int)(Math.random() * 5); //sets random item for weapon option b
+    //c = (int)(Math.random() * 5); //sets random item for weapon option c
+    //while(b == a){b = (int)(Math.random() * 5);} //make sure b is not the same as a
+    //while(c == a || c == b){c = (int)(Math.random() * 5);} //make sure c is not the same as a or b     
+    //System.out.println(">>  a) " + Profession[a] + " b) " + Profession[b] + " c) " + Profession[c]);//shows choices
+    //int select = 0;
+    //while(select == 0){ //while option hasnt been selected
+    //    switch(input.next()) { //test to see which option was selected
+    //        case "a": Character.profession = Profession[a]; //if option A, set A as weapon
+    //                  select = 1;  //option has been selected
+    //                  break;
+    //        case "b": Character.profession = Profession[b]; //if option B, set B as weapon
+    //                 select = 1;  //option has been selected
+    //                  break;
+    //        case "c": Character.profession = Profession[c]; //if option C, set C as weapon
+    //                  select = 1;  //option has been selected
+    //                  break;
+    //    }
+    //}
+    //********************************************************************************************************************//
+    Character.create();
     System.out.println(">>you have chosen the profession " + Character.profession); 
-            
-    switch(Character.profession) { //Check Profession and apply Specialisations
-        case "Sorceror" : System.out.println(">>As a Sorceror you gain +4 MAGIC and can choose 2 spells");
-                          Character.MAGIC = Character.MAGIC + 4;
-                          Character.spells = Character.spells + 2;
-            break;
-        case "Rogue"    : System.out.println(">>As a Rogue you gain +3 STEALTH and +2 DEXTERITY");
-                          Character.STEALTH = Character.STEALTH + 3;
-                          Character.DEXTERITY = Character.DEXTERITY + 2;
-            break;
-        case "Bard"     : System.out.println(">>As a Bard you gain +4 INFLUENCE and an instrument");
-                          Character.INFLUENCE = Character.INFLUENCE + 4;
-                          Character.Equiptment.add("Instrument");
-            break;
-        case "Warrior"  : System.out.println(">>As a Warrior you gain +4 STRENGTH and +1 DEXTERITY");
-                          Character.STRENGTH = Character.STRENGTH + 4;
-                          Character.DEXTERITY = Character.DEXTERITY + 1;
-            break;
-        case "Chef"     : System.out.println(">>As a Chef you gain +4 FOOD and +1 INFLUENCE");
-                          Character.FOOD = Character.FOOD + 4;
-                          Character.INFLUENCE = Character.INFLUENCE + 1;
-            break;
-        }
-    
-    
     System.out.println(">>");
     System.out.println(">>Your Character so far:");
     System.out.println(">>Name: " + Character.name);
     System.out.println(">>Profession: " + Character.profession);
     System.out.println(">>Weapon: " + Character.weapon);
+    System.out.println(">>STRENGTH: " + Character.STRENGTH);
+    System.out.println(">>DEXTERITY: " + Character.DEXTERITY);
+    System.out.println(">>STEALTH: " + Character.STEALTH);
+    System.out.println(">>INFLUENCE: " + Character.INFLUENCE);
+    System.out.println(">>MAGIC: " + Character.MAGIC);
+    System.out.println(">>FOOD: " + Character.FOOD);
+
+    }
     }
 }
+
+
     
 
